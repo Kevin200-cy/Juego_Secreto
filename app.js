ad8f7ff06500fab224e3d6bbb62a0f5c7852660d
Kevin Anibal <kevinhuamanisilvera9@gmail.com>
2025-02-04 11:05:00 -0500

    AGregar y Listar amigos

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const lista = document.getElementById("listaAmigos");
+
+    if (input.value.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(input.value.trim());
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    lista.appendChild(li);
+
+    input.value = "";
+}
+
